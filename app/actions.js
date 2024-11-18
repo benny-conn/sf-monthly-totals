35ca4a8c3e2ec6e6480e8492c2313d3877ca75de
Benjamin Conn <benny@bennyc.io>
2024-11-18 17:13:19 -0500

    more combinations

--- app/actions.js
+++ app/actions.js
@@ -29,10 +29,17 @@ const subProjectMappings = {
       "Double LP [two 180-g discs, numbered, edition of 500]",
     "Limited Edition 180-gram vinyl LP":
       "Double LP [two 180-g discs, numbered, edition of 500]",
+    "Digital Album Download [multiple formats]":
+      "High-res digital download FLAC or WAV",
   },
   "World Travelers": {
     "LP [180-g]": "Limited Edition 180-gram vinyl LP",
     "180-Gram LP": "Limited Edition 180-gram vinyl LP",
+    "Digital Album Download [multiple formats]":
+      "High-res digital download FLAC or WAV",
+  },
+  Humanoid: {
+    "LP [180-g, numbered, edition of 500]": "Limited Edition 180-gram vinyl LP",
   },
 }
 
